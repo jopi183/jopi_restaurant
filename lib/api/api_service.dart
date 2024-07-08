2c5027710feb52a6ac6905f1d895791807da5a02
Joshua Pinem <joshuapinem183@gmail.com>
2024-07-08 18:53:17 +0700

    update

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,41 +1,39 @@
 import 'dart:convert';
-import 'dart:ui';
 import 'package:http/http.dart' as http;
 import 'package:jopi_restaurant/model/restaurant.dart';
 import 'package:flutter/material.dart';
 
-Class ApiService{
+class ApiService {
   static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
   static const String _list = 'list';
-  static const String _detail = 'detail/:id';
-  static const String _search = 'search?q=<query>';
+  static const String _detail = 'detail/';
+  static const String _search = 'search?q=';
   static const String _review = 'review';
 
-  Future<Restaurant> headRestaurant() async{
-    final response = await http.get(Uri.parse("${_baseUrl} + ${_list}"));
-    if (response.statusCode == 200){
-      return Restaurant.fromJson(json.decode(response.body));
-    }else{
+  Future<ListRestaurant> fetchRestaurantList() async {
+    final response = await http.get(Uri.parse("$_baseUrl$_list"));
+    if (response.statusCode == 200) {
+      return ListRestaurant.fromJson(json.decode(response.body));
+    } else {
       throw Exception('Failed to load restaurants');
     }
   }
 
-Future<Restaurant> headRestaurant() async{
-  final response = await http.get(Uri.parse("${_baseUrl} + ${_list}"));
-  if (response.statusCode == 200){
-    return Restaurant.fromJson(json.decode(response.body));
-  }else{
-    throw Exception('Failed to load restaurants');
+  Future<DetailRestaurant> fetchRestaurantDetail(String id) async {
+    final response = await http.get(Uri.parse("$_baseUrl$_detail$id"));
+    if (response.statusCode == 200) {
+      return DetailRestaurant.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant details');
+    }
   }
-}
 
-
-Future <Restaurant> searchRestaurant(String query) async{
-    final response = await http.get(Uri.parse("${_baseUrl} + ${_search} + $query"));
-    if (response.statusCode == 200){
-      return Restaurant.fromJson(json.decode(response.body));
-    }else{
-      throw Exception('Restaurant not Found');
+  Future<SearchRestaurant> searchRestaurant(String query) async {
+    final response = await http.get(Uri.parse("$_baseUrl$_search$query"));
+    if (response.statusCode == 200) {
+      return SearchRestaurant.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Restaurant not found');
     }
   }
 }
