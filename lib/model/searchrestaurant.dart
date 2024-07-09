6d14bdbbc44431613c9a3fe3c2b38c9e1f421f8d
Joshua Pinem <joshuapinem183@gmail.com>
2024-07-09 13:03:21 +0700

    add new file

---
+++ lib/model/searchrestaurant.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+class SearchRestaurant {
+  bool error;
+  int founded;
+  List<Restaurant> restaurants;
+
+  SearchRestaurant({
+    required this.error,
+    required this.founded,
+    required this.restaurants,
+  });
+
+  factory SearchRestaurant.fromRawJson(String str) => SearchRestaurant.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory SearchRestaurant.fromJson(Map<String, dynamic> json) => SearchRestaurant(
+    error: json["error"],
+    founded: json["founded"],
+    restaurants: List<Restaurant>.from(json["restaurants"].map((x) => Restaurant.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "error": error,
+    "founded": founded,
+    "restaurants": List<dynamic>.from(restaurants.map((x) => x.toJson())),
+  };
+}
+
+class Restaurant {
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  double rating;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+  });
+
+  factory Restaurant.fromRawJson(String str) => Restaurant.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
+    id: json["id"],
+    name: json["name"],
+    description: json["description"],
+    pictureId: json["pictureId"],
+    city: json["city"],
+    rating: json["rating"]?.toDouble(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "description": description,
+    "pictureId": pictureId,
+    "city": city,
+    "rating": rating,
+  };
+}
